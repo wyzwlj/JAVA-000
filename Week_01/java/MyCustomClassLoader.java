e2d98fb062253b1b32e3dd119d015cc6cf9665c7
Administrator <1048978745@qq.com>
2020-10-22 07:24:35 +0800

    refactor

--- Week_01/java/MyCustomClassLoader.java
+++ Week_01/java/MyCustomClassLoader.java
@@ -1,29 +1,30 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 
 public class MyCustomClassLoader extends ClassLoader {
 
     public Class<?> findClass(final String name) throws ClassNotFoundException {
         if ("Hello".equals(name)) {
-            byte[] classBytes = getClassBytes();
-            return defineClass(name, classBytes, 0, classBytes.length);
-        } else {
-            return super.findClass(name);
+            try {
+                ClassByteData classByteData = getCustomClassByteData();
+                return defineClass(name, classByteData.bytes, 0, classByteData.length);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
         }
+        return super.findClass(name);
     }
 
-    protected byte[] getClassBytes() {
-        byte[] bytes = new byte[1024];
-        File file = null;
-        try {
-            file = new File(MyCustomClassLoader.class.getClassLoader().getResource("Hello.xlass").toURI());
-        } catch (URISyntaxException e) {
-            return bytes;
-        }
+    private ClassByteData getCustomClassByteData() throws URISyntaxException, IOException {
+        ClassByteData classByteData = new ClassByteData();
+        File file = new File(getCustomClassPath());
         try (FileInputStream inputStream = new FileInputStream(file)) {
             int index = 0;
+            byte[] bytes = new byte[1024];
             while (true) {
                 int read = inputStream.read();
                 if (read != -1) {
@@ -32,10 +33,19 @@ public class MyCustomClassLoader extends ClassLoader {
                     break;
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            classByteData.bytes = bytes;
+            classByteData.length = index;
         }
-        return bytes;
+        return classByteData;
+    }
+
+    private URI getCustomClassPath() throws URISyntaxException {
+        return MyCustomClassLoader.class.getClassLoader().getResource("Hello.xlass").toURI();
+    }
+
+    class ClassByteData {
+        private byte[] bytes;
+        private int length;
     }
 
     public static void main(String[] args) throws Exception {
